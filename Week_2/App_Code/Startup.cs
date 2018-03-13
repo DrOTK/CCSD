28f9402a1cb799adbd813cb5dd47168fcb2e6779
DrOTK <36900805+DrOTK@users.noreply.github.com>
2018-03-13 13:48:53 -0700

    Initial Commit for PRG 340
    Initial Commit for PRG 340

---
+++ Week_2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CCSD.Startup))]
+namespace CCSD
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
